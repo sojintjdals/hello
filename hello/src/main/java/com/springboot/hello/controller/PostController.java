39c2b384f23b7b04737a6f91e043b0fc105380c9
sojintjdals <49427469+sojintjdals@users.noreply.github.com>
2022-10-01 15:41:39 +0900

    Add files via upload

---
+++ hello/src/main/java/com/springboot/hello/controller/PostController.java
@@ -0,0 +1,69 @@
+package com.springboot.hello.controller;
+
+import com.springboot.hello.dto.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/post-api")
+public class PostController {
+    @RequestMapping(value = "/domain", method = RequestMethod.POST)
+    public String postExample(){
+        return "Hello Post API";
+    }
+
+// PostMapping
+//    @PostMapping(value = "/member")
+//    public String postMember(@RequestBody Map<String, Object> postData){
+//        StringBuilder sb = new StringBuilder();
+//
+//        postData.entrySet().forEach(map ->{
+//            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+//        });
+//
+//        return sb.toString();
+//    }
+
+//    @PostMapping(value="/member2")
+//    public String postMemberDto(@RequestBody MemberDto memberDto){
+//        return memberDto.toString();
+//    }
+
+//PutMapping
+    @PutMapping(value = "/member")
+    public String postMember(@RequestBody Map<String, Object> putData){
+        StringBuilder sb = new StringBuilder();
+
+        putData.entrySet().forEach(map ->{
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    @PutMapping(value="/member1")
+    public String postMemberDto1(@RequestBody MemberDto memberDto){
+        return memberDto.toString();
+    }
+
+    @PutMapping(value="/member2")
+    public MemberDto postMemberDto2(@RequestBody MemberDto memberDto){
+        return memberDto;
+    }
+    //responseEntity
+    @PutMapping(value="/member3")
+    public ResponseEntity<MemberDto> postMemberDto3(@RequestBody MemberDto memberDto){
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(memberDto);
+    }
+
+    // delete
+    @DeleteMapping(value = "/request1")
+    public String getRequestParam1(@RequestParam String email){
+        return "e-mail : " + email;
+    }
+}
